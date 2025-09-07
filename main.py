6c7ca6d46c0f694f91aca1ad3f99350ae36acced
VINAYAKASAI-6701 <vinaysai6701@gmail.com>
2025-09-07 19:49:21 +0530

    validated schema using pydantic

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app=FastAPI()
+
+
+class Post(BaseModel):
+    title:str
+    content:str
+    published:bool=True
+    rating:Optional[int]=None
+
 
 @app.get("/")#we are hitting the api endpoint using get method
 def root():
@@ -9,3 +20,11 @@ def root():
 @app.get("/posts")
 def get_posts():
     return {"data":"This is my data"}
+
+@app.post("/createposts")
+def create_posts(new_post:Post):
+    print(new_post)
+    print(new_post.dict())
+    return {"data":new_post}
+#we expect data that have title adn string,category of post,Boolean
+
